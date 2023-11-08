882b2b70295a636a6440b91d81bf12cdf0d745b6
PhatStraw <kevindsimsjr@gmail.com>
2023-11-08 14:05:01 -0800

    email-sending

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const { google } = require('googleapis');
 const fs = require('fs');
 const OpenAI = require('openai');
+const nodemailer = require('nodemailer');
 const TOKEN_PATH = 'token.json';
 const OUTPUT_PATH = 'output.json';
 
@@ -112,8 +113,57 @@ async function summarizeContent() {
     messages: [
         {
           role: "system",
-          content: `you are an ai assistant hired by parents to monitor their childrens internet exposure. 
-          given the following content from youtube, analyze and create a json object that contains a personal email newsletter that will be sent to the parent whose childs content this is and split the contents of that newletter into seperate objects on the response that can be displayed to the user via an api call. give opinion on the psych of the child and topics to discuss with the child.`,
+          content: `
+          you are an ai assistant hired by parents to monitor their 
+          childrens internet exposure. 
+
+          given the following content from youtube, analyze 
+          and create a json object that contains a personal 
+          email newsletter that will be sent to the parent
+          whose childs content this is and split the contents 
+          of that newletter into seperate objects on the response 
+          that can be displayed to the user via an api call. 
+          give opinion on the psych of the child and topics to 
+          discuss with the child.
+          
+          response should be in the following format:
+          {
+            "email_newsletter": {
+              "subject": "Your Child's Recent YouTube Activity - Insightful Overview and Discussion Topics",
+              "greeting": "Dear Parent,",
+              "introduction": "",
+              "content_analysis": [
+                {
+                  "topic": "",
+                  "content": ""
+                },
+                {
+                  "topic": "",
+                  "content": ""
+                },
+                {
+                  "topic": "",
+                  "content": ""
+                },
+                {
+                  "topic": "",
+                  "content": "."
+                }
+              ],
+              "psych_analysis": {
+                "overview": "",
+                "topics_to_discuss": [
+                  "",
+                  "",
+                  "",
+                  ""
+                ]
+              },
+              "closing": "",
+              "sign_off": ""
+            }
+          }
+          `,
         },
         {
             role: "user",
@@ -121,10 +171,106 @@ async function summarizeContent() {
           },
     ]
   });
-  console.log(response.choices[0].message.content)
+  const parsed = JSON.parse(response.choices[0].message.content)
+  console.log(parsed)
 
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'kevindsims1@gmail.com',
+      pass: 'azzz amet ydyd qmjo'
+    }
+  });
+
+  let mailOptions = {
+    from: 'kevindsims1@gmail.com',
+    to: 'Willscottmoss@gmail.com',
+    subject: "Your Child's Recent YouTube Activity - Insightful Overview and Discussion Topics",
+    html: `
+    <html>
+      <head>
+        <style>
+          /* Add your custom CSS styles here */
+        </style>
+      </head>
+      <body>
+        <h1>Your Child's Recent YouTube Activity</h1>
+        <p>Dear Parent,</p>
+        <p>${parsed.email_newsletter.introduction}</p>
+        
+        <h2>Content Analysis</h2>
+        <ul>
+          ${parsed.email_newsletter.content_analysis.map(analysis => `
+            <li>
+              <h3>${analysis.topic}</h3>
+              <p>${analysis.content}</p>
+            </li>
+          `).join('')}
+        </ul>
+        
+        <h2>Psychological Analysis</h2>
+        <p>${parsed.email_newsletter.psych_analysis.overview}</p>
+        <h3>Topics to Discuss:</h3>
+        <ul>
+          ${parsed.email_newsletter.psych_analysis.topics_to_discuss.map(topic => `
+            <li>${topic}</li>
+          `).join('')}
+        </ul>
+        
+        <p>${parsed.email_newsletter.closing}</p>
+        <p>${parsed.email_newsletter.sign_off}</p>
+      </body>
+    </html>
+  `
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
   // Return the summary in JSON format
 //   return {
 //     summary: response.choices[0].text.trim()
 //   };
 }
+
+
+// {
+//     "email_newsletter": {
+//       "subject": "Your Child's Recent YouTube Activity - Insightful Overview and Discussion Topics",
+//       "greeting": "Dear Parent,",
+//       "introduction": "We hope this email finds you well. As part of our ongoing commitment to support your child’s healthy internet usage, we present you with an analysis of their recent YouTube activities. It is designed to give you insights into their interests and how they might be spending their online time.",
+//       "content_analysis": [
+//         {
+//           "topic": "Entrepreneurship and Startups",
+//           "content": "Your child has shown an interest in startup culture and entrepreneurship. Videos from Y Combinator, MicroConf, and various founders indicate a curiosity about business formation, growth, and the tech industry. The focus on SaaS (Software as a Service) startups suggests a specific interest in this area."
+//         },
+//         {
+//           "topic": "Personal Development",
+//           "content": "Content liked by your child includes tips on productivity, personal growth, and making impactful life choices. This could reflect a mature approach to self-improvement and time management at a young age."
+//         },
+//         {
+//           "topic": "Technology and Innovation",
+//           "content": "There is a clear inclination towards technology, specifically in areas like AI, web development, and software engineering. Your child may be exploring tech as a field of study or a potential career path."
+//         },
+//         {
+//           "topic": "Financial Literacy",
+//           "content": "Engagement with videos from True Link Financial indicates an interest in financial independence and perhaps a consideration for people with special requirements. This insight could denote empathy as well as financial curiosity."
+//         }
+//       ],
+//       "psych_analysis": {
+//         "overview": "The content liked by your child points towards a high level of intellectual curiosity, especially in areas of high cognitive demand such as entrepreneurship and technology. They may possess a forward-thinking mindset, and their interest in self-improvement material may indicate an understanding of the value of personal growth.",
+//         "topics_to_discuss": [
+//           "Aspirations within the realms of entrepreneurship and whether they have considered creating or leading projects of their own.",
+//           "Their thoughts on personal productivity and if they have implemented any strategies from their viewing materials.",
+//           "Understanding of financial concepts and whether they have thoughts on financial planning or independence.",
+//           "Technology's role in society and ethical considerations, especially in regards to AI, which appears to be of interest."
+//         ]
+//       },
+//       "closing": "It is wonderful to see your child engaging with content that could be shaping a growth-oriented and innovative mindset. We encourage you to discuss these topics with them; such conversations can provide guidance and support as they explore their potential. If you have any questions or need further resources to facilitate these discussions, please do not hesitate to reach out.",
+//       "sign_off": "Warm regards,\nYour Dedicated AI Assistant"
+//     }
+//   }
